e8b5f7e7c788ced988de18c0e66614350039af9c
sainikhil029 <sainikhill029@gmail.com>
2024-11-13 15:38:08 +0530

    this is my 1st commit

---
+++ src/controllers/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    InfoControllers: require('./info-controllers')
+};  
---
+++ src/routes/v1/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const{InfoControllers} = require('../../controllers');
+
+const router = express.Router();
+
+router.get('/info',InfoControllers.info);
+
+
+module.exports = router; 
